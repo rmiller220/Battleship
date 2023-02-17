b70f947d4747ab6e6ff3ec7911e56be752486493
Sean Cowans <zedesia@yahoo.com>
2023-02-17 14:28:12 -0700

    Correct valid placement and ship overlap

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -61,7 +61,6 @@ RSpec.describe Board do
       cell_1 = board.cells["A1"] 
       cell_2 = board.cells["A2"]
       cell_3 = board.cells["A3"]
-
       expect(cell_1.ship).to eq(cruiser)
       expect(cell_2.ship).to eq(cruiser)
       expect(cell_3.ship).to eq(cruiser)
@@ -70,10 +69,11 @@ RSpec.describe Board do
     it 'checks overlapping placement of ships' do
       board = Board.new
       cruiser = Ship.new("Cruiser", 3)
-      board.place(cruiser, ["A1", "A2", "A3"])
       submarine = Ship.new("Submarine", 2)
 
+      expect(board.place(cruiser, ["A1", "A2", "A3"])).to be(true)
       expect(board.valid_placement?(submarine, ["A1", "B1"])).to be(false)
+      expect(board.place(submarine, ["A1", "B1"])).to be(false)
     end
   end
 end
