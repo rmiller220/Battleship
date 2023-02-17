63d21bdfc9dce7f1f951e06a56d8bf5ca0c338d6
Sean Cowans <zedesia@yahoo.com>
2023-02-16 18:03:07 -0700

    Add fire_upon and fired_upon? methods to cell clas

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,12 @@
 class Cell
 attr_reader :coordinate,
-            :ship
+            :ship,
+            :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
@@ -17,5 +19,18 @@ attr_reader :coordinate,
 
   def place_ship(ship)
     @ship = ship
+  end
+  
+  def fire_upon
+    if @fired_upon = false
+      @fire_upon = true
+      @ship.health -= 1
+    else
+      p "Can't fire"
+    end
+  end
+
+  def fired_upon?
+    @fired_upon
   end
 end
